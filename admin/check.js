7a0e6618c45f433a998207a87fad2f48ac199e80
student777 <darkdakku@gmail.com>
2020-06-01 15:03:51 +0900

    Add admin run_list

--- admin/check.js
+++ admin/check.js
@@ -1,20 +1,36 @@
 function check() {
-    function log() {
-        var json = JSON.parse(this.responseText);
-        document.getElementById('output').innerText = json.sort((a, b) => a.name.localeCompare(b.name)).map(u => u.name).join(', ');
-    }
-    ajax("/BRCassemble/server/admin.user.list.json", log);
-}
-
-function ajax(url, callback) {
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", callback);
-    oReq.open("GET", url);
-    oReq.send();
+    ajax("/BRCassemble/server/admin.run.latest.json", (res) => {
+        var json = JSON.parse(res);
+        document.getElementById('output').innerText = json.users.sort((a, b) => a.name.localeCompare(b.name)).map(u => u.name).join(', ');
+    });
 }
 
 function addMember() {
     var name = document.getElementById('name').value;
     document.getElementById('output').innerText += ", " + name;
     document.getElementById('name').value = '';
+}
+
+function ajax(url, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status) {
+            var res = httpRequest.responseText;
+            callback(res);
+        }
+    };
+    httpRequest.open("GET", url);
+    httpRequest.send();
+};
+
+function prevRunning() {
+    var run_list = document.getElementById('run_list');
+    ajax("/BRCassemble/server/admin.run.list.json", (res) => {
+        var json = JSON.parse(res);
+        json.forEach(r => {
+            var child = document.createElement('span');
+            child.innerText = r.name + '@' + r.time;
+            run_list.appendChild(child);
+        })
+    });
 }
